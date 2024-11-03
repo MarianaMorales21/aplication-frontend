e6a9839807cc2866f721da433cd90f993b14d1da
Mariana Morales <marianamorales2110@gmail.com>
2024-11-03 02:39:01 -0400

    Arreglos de los modulos de flota y materiales

--- src/views/flota/Flota.js
+++ src/views/flota/Flota.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { 
-    CTable, 
-    CTableBody, 
-    CTableHead, 
-    CTableHeaderCell, 
-    CTableRow, 
-    CTableDataCell, 
-    CButton, 
-    CNavbar,
-    CForm,
-    CFormInput, 
-    CContainer
- } from '@coreui/react';
+  CTable, 
+  CTableBody, 
+  CTableHead, 
+  CTableHeaderCell, 
+  CTableRow, 
+  CTableDataCell, 
+  CButton, 
+  CNavbar,
+  CForm,
+  CFormInput, 
+  CContainer,
+  CModal, 
+  CModalHeader, 
+  CModalTitle, 
+  CModalBody, 
+  CModalFooter,
+  CCol, 
+  CFormSelect
+} from '@coreui/react';
 const Flota = () => {
+    const [visible, setVisible] = useState(false);
+    const [visibleSm, setVisibleSm] = useState(false);
   return (
     <div>
       <h1>List of Flota</h1>
@@ -55,8 +64,8 @@ const Flota = () => {
             <CTableDataCell>{'250Km'}</CTableDataCell>
             <CTableDataCell>{'Activo'}</CTableDataCell>
             <CTableDataCell>
-            <CButton style={{backgroundColor:'green', marginRight:'10px', color:'white'}}>Edit</CButton>
-            <CButton style={{backgroundColor:'red', marginRight:'10px', color:'white'}}>Delete</CButton>
+            <CButton style={{backgroundColor:'green', marginRight:'10px', color:'white'}} onClick={() => setVisibleSm(!visibleSm)}>Edit</CButton>
+            <CButton style={{backgroundColor:'red', marginRight:'10px', color:'white'}} onClick={() => setVisible(true)}>Delete</CButton>
             </CTableDataCell>
           </CTableRow>
           <CTableRow>
@@ -67,15 +76,102 @@ const Flota = () => {
             <CTableDataCell>{'500Km'}</CTableDataCell>
             <CTableDataCell>{'Inavilitado'}</CTableDataCell>
             <CTableDataCell>
-            <CButton style={{backgroundColor:'green', marginRight:'10px', color:'white'}}>Edit</CButton>
-            <CButton style={{backgroundColor:'red', marginRight:'10px', color:'white'}}>Delete</CButton>
+            <CButton style={{backgroundColor:'green', marginRight:'10px', color:'white'}} onClick={() => setVisibleSm(!visibleSm)}>Edit</CButton>
+            <CButton style={{backgroundColor:'red', marginRight:'10px', color:'white'}} onClick={() => setVisible(true)}>Delete</CButton>
             </CTableDataCell>
           </CTableRow>
         </CTableBody>
       </CTable>
-      <CButton type="submit" style={{backgroundColor:'#107acc', borderColor:'black', color:'white'}} variant="outline">
-      New Member
-      </CButton>
+
+      <CModal visible={visible} onClose={() => setVisible(false)}>
+            <CModalHeader>
+            <CModalTitle>Attention</CModalTitle>
+            </CModalHeader>
+            <CModalBody>Are you sure to remove this member from the system?</CModalBody>
+            <CModalFooter>
+            <CButton style={{ backgroundColor: 'green', marginRight: '10px', color: 'white', borderColor: 'black' }} onClick={() => setVisible(false)}>
+                Cancel
+            </CButton>
+            <CButton style={{ backgroundColor: 'red', marginRight: '10px', color: 'white', borderColor: 'black' }} onClick={() => setVisible(false)}>
+                Delete
+            </CButton>
+            </CModalFooter>
+            </CModal>
+      <CModal size="sm" visible={visibleSm} onClose={() => setVisibleSm(false)}>
+        <CModalHeader>
+          <CModalTitle>Edit Member</CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+            <h6>Nro. User: 5012</h6>
+            <h6>Truck License Plate: AA58G4</h6>
+            <h6>Name Driver: Mariana Morales</h6>
+        <CForm className="row g-3">
+            <CCol md={12}>
+                <CFormSelect id="status" label="Operational Status" style={{borderColor:'black'}}>
+                <option>Choose...</option>
+                <option>Active</option>
+                <option>Inactive</option>
+                <option>Service</option>
+                </CFormSelect>
+            </CCol>
+            <CCol md={12} >
+                <CFormInput placeholder="Miliage" id="Miliage" label="Miliage" style={{borderColor:'black'}}/>
+            </CCol>
+            <CCol md={6}>
+                <CButton style={{backgroundColor:'red', borderColor:'black', color:'white', marginBottom:'10px'}} type="submit" onClick={() => setVisibleLg(false)} >
+                    Cancel
+                </CButton>
+                <CButton style={{backgroundColor:'#107acc', borderColor:'black', color:'white'}} type="submit" onClick={() => setVisible(!visible)}>Add Changes</CButton>
+            </CCol>
+        </CForm>
+        </CModalBody>
+      </CModal>
+      <h2>New Member</h2>
+      <div>
+      <CNavbar style={{border: '1px solid gray', borderRadius:'10px', marginBottom:'10px', backgroundColor:'white'}}>
+        <CContainer>
+            <CForm>    
+            <CCol md={12}>
+                <CFormInput placeholder="Name User" id="Name" label="Name User" type='search' style={{borderColor:'black'}}/>
+            </CCol>
+            <h6 style={{marginTop:'15px'}}>Data Found:</h6>
+              <p>
+                Name:<br></br>          
+                DNI:<br/>
+                Email: <br/>
+                Phone:<br/>
+                Address:<br/>
+              </p>
+              <div style={{borderTop: '2px solid black', width:'800px'}}>
+              <CCol md={6}>
+                <CFormSelect id="status" label="Operational Status" style={{borderColor:'black'}}>
+                <option>Choose...</option>
+                <option>Active</option>
+                <option>Inactive</option>
+                <option>Service</option>
+                </CFormSelect>
+            </CCol>
+            <CCol md={6} >
+                <CFormInput placeholder="Miliage" id="Miliage" label="Miliage" style={{borderColor:'black'}}/>
+            </CCol>
+            <CCol md={6} >
+                <CFormInput placeholder="Usage Reports" id="Usage Reports" label="Usage Reports" style={{borderColor:'black'}}/>
+            </CCol>
+            <CCol md={6} >
+                <CFormInput placeholder="Model" id="Model" label="Model" style={{borderColor:'black'}}/>
+            </CCol>
+            <CCol md={6} >
+                <CFormInput placeholder="Brand" id="Brand" label="Brand" style={{borderColor:'black'}}/>
+            </CCol>
+              </div>
+              <CButton type="submit" style={{backgroundColor:'#107acc', borderColor:'black', color:'white', marginTop:'15px'}} 
+              variant="outline">
+              Add New Member
+              </CButton>
+            </CForm>
+        </CContainer>
+        </CNavbar>
+      </div>
     </div>
   );
 };
--- src/views/materials/Materials.js
+++ src/views/materials/Materials.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { 
-    CTable, 
-    CTableBody, 
-    CTableHead, 
-    CTableHeaderCell, 
-    CTableRow, 
-    CTableDataCell, 
-    CButton, 
-    CNavbar,
-    CForm,
-    CFormInput, 
-    CContainer
- } from '@coreui/react';
+  CTable, 
+  CTableBody, 
+  CTableHead, 
+  CTableHeaderCell, 
+  CTableRow, 
+  CTableDataCell, 
+  CButton, 
+  CNavbar,
+  CForm,
+  CFormInput, 
+  CContainer,
+  CModal, 
+  CModalHeader, 
+  CModalTitle, 
+  CModalBody, 
+  CModalFooter,
+  CCol, 
+  CFormSelect
+} from '@coreui/react';
 const Materials = () => {
+  const [visible, setVisible] = useState(false);
+  const [visibleSm, setVisibleSm] = useState(false);
+  const [visibleLg, setVisibleLg] = useState(false);
   return (
     <div>
       <h1>List of Materials</h1>
@@ -47,8 +57,8 @@ const Materials = () => {
             <CTableDataCell>{'Rocas arenozas'}</CTableDataCell>
             <CTableDataCell>{'Disponible'}</CTableDataCell>
             <CTableDataCell>
-            <CButton style={{backgroundColor:'green', marginRight:'10px', color:'white'}}>Edit</CButton>
-            <CButton style={{backgroundColor:'red', marginRight:'10px', color:'white'}}>Delete</CButton>
+            <CButton style={{backgroundColor:'green', marginRight:'10px', color:'white'}} onClick={() => setVisibleSm(!visibleSm)}>Edit</CButton>
+            <CButton style={{backgroundColor:'red', marginRight:'10px', color:'white'}} onClick={() => setVisible(true)}>Delete</CButton>
             </CTableDataCell>
           </CTableRow>
           <CTableRow>
@@ -58,15 +68,95 @@ const Materials = () => {
             <CTableDataCell>{'Arena caracterizada por su color rojo'}</CTableDataCell>
             <CTableDataCell>{'Agotado'}</CTableDataCell>
             <CTableDataCell>
-            <CButton style={{backgroundColor:'green', marginRight:'10px', color:'white'}}>Edit</CButton>
-            <CButton style={{backgroundColor:'red', marginRight:'10px', color:'white'}}>Delete</CButton>
+            <CButton style={{backgroundColor:'green', marginRight:'10px', color:'white'}} onClick={() => setVisibleSm(!visibleSm)}>Edit</CButton>
+            <CButton style={{backgroundColor:'red', marginRight:'10px', color:'white'}} onClick={() => setVisible(true)}>Delete</CButton>
             </CTableDataCell>
           </CTableRow>
         </CTableBody>
       </CTable>
-      <CButton type="submit" style={{backgroundColor:'#107acc', borderColor:'black', color:'white'}} variant="outline">
+      <CButton type="submit" style={{backgroundColor:'#107acc', borderColor:'black', color:'white'}} variant="outline" onClick={() => setVisibleLg(!visibleLg)}>
        New Material
         </CButton>
+        <CModal size="lg" visible={visibleLg} onClose={() => setVisibleLg(false)}>
+        <CModalHeader>
+          <CModalTitle>New Material</CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+        <CForm className="row g-3">
+            <CCol md={6} >
+                <CFormInput placeholder="Name" id="NameId" label="Name Material" style={{borderColor:'black'}}/>
+            </CCol>
+            <CCol md={6}>
+                <CFormInput placeholder="Characteristics" id="Characteristics" label="Characteristics" style={{borderColor:'black'}}/>
+            </CCol>
+            <CCol md={6}>
+                <CFormInput id="Cost per Unit" label="Cost per Unit" placeholder="Cost per Unit" style={{borderColor:'black'}}/>
+            </CCol>
+            <CCol md={6}>
+                <CFormInput id="Cost per Kilometer" label="Cost per Kilometer" placeholder="Cost per Kilometert" style={{borderColor:'black'}}/>
+            </CCol>
+            <CCol md={12}>
+              <CFormSelect id="status" label="Status" placeholder="Status" style={{borderColor:'black'}}>
+                <option>Choose...</option>
+                <option>Available </option>
+                <option>Rough</option>
+              </CFormSelect>
+            </CCol>
+            <CCol md={6}>
+                <CButton style={{backgroundColor:'red', borderColor:'black', color:'white', marginRight:'10px'}} type="submit" onClick={() => setVisibleLg(false)} >
+                    Cancel
+                </CButton>
+                <CButton style={{backgroundColor:'#107acc', borderColor:'black', color:'white'}} type="submit" onClick={() => setVisible(!visible)}>Add Invoice</CButton>
+            </CCol>
+        </CForm>
+        </CModalBody>
+      </CModal>
+      <CModal visible={visible} onClose={() => setVisible(false)}>
+            <CModalHeader>
+            <CModalTitle>Attention</CModalTitle>
+            </CModalHeader>
+            <CModalBody>Are you sure to remove this material from the system?</CModalBody>
+            <CModalFooter>
+            <CButton style={{ backgroundColor: 'green', marginRight: '10px', color: 'white', borderColor: 'black' }} onClick={() => setVisible(false)}>
+                Cancel
+            </CButton>
+            <CButton style={{ backgroundColor: 'red', marginRight: '10px', color: 'white', borderColor: 'black' }} onClick={() => setVisible(false)}>
+                Delete
+            </CButton>
+            </CModalFooter>
+       </CModal>
+
+       <CModal size="sm" visible={visibleSm} onClose={() => setVisibleSm(false)}>
+        <CModalHeader>
+          <CModalTitle>Edit Material</CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+            <h6>Nro. Material: 5012</h6>
+            <h6>Name Material: Arena Roja</h6>
+        <CForm className="row g-3">
+          <CCol md={6}>
+                <CFormInput id="Cost per Unit the Material" label="Cost per Unit (kilograms)" placeholder="Cost per Unit" style={{borderColor:'black'}}/>
+            </CCol>
+            <CCol md={6}>
+                <CFormInput id="Cost per Kilometer" label="Cost per Kilometer" placeholder="Cost per Kilometert" style={{borderColor:'black'}}/>
+            </CCol>
+
+            <CCol md={12}>
+              <CFormSelect id="status" label="Status" placeholder="Status" style={{borderColor:'black'}}>
+                <option>Choose...</option>
+                <option>Available </option>
+                <option>Rough</option>
+              </CFormSelect>
+            </CCol>
+            <CCol md={6}>
+                <CButton style={{backgroundColor:'red', borderColor:'black', color:'white', marginBottom:'10px'}} type="submit" onClick={() => setVisibleLg(false)} >
+                    Cancel
+                </CButton>
+                <CButton style={{backgroundColor:'#107acc', borderColor:'black', color:'white'}} type="submit" onClick={() => setVisible(!visible)}>Add Changes</CButton>
+            </CCol>
+        </CForm>
+        </CModalBody>
+      </CModal>
     </div>
   );
 };
